1fceae8e1538c318d4aadf0c9ab28eb6a007319b
mjamecny <jamecny.m@tutanota.com>
2023-11-21 15:19:21 +0100

    add DarkModeContext

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+const DarkModeContext = createContext()
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark")
+      } else {
+        document.documentElement.classList.remove("dark")
+      }
+    },
+    [isDarkMode]
+  )
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark)
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  )
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext)
+  if (context === undefined) {
+    throw new Error("DarkModeContext used outside of DarkModeProvider")
+  }
+
+  return context
+}
+
+export { DarkModeProvider, useDarkMode }
